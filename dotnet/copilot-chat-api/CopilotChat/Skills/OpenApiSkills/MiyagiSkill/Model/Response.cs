6295dba69ecceb20103204a161493826dce8b542
Govind Kamtamneni <gok@microsoft.com>
2023-06-06 15:09:35 -0700

    add miyagi openapi skill

---
+++ dotnet/copilot-chat-api/CopilotChat/Skills/OpenApiSkills/MiyagiSkill/Model/Response.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Text.Json.Serialization;
+
+namespace SemanticKernel.Service.CopilotChat.Skills.OpenApiSkills.MiyagiSkill.Model;
+
+/// <summary>
+/// Represents a Response.
+/// </summary>
+public class Response
+{
+    /// <summary>
+    /// Gets or sets the Response message.
+    /// </summary>
+    [JsonPropertyName("message")]
+    public string Message { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Response"/> class.
+    /// </summary>
+    /// <param name="message">Response message</param>
+    public Response(string message)
+    {
+        this.Message = message;
+    }
+}
