2e249d2accfde986bc51b6fd6ae19957f3f74011
Govind Kamtamneni <gok@microsoft.com>
2023-06-28 15:46:31 -0700

    bump sk version and refactor kernel builder

--- dotnet/recommendation-service/Program.cs
+++ dotnet/recommendation-service/Program.cs
@@ -1,5 +1,6 @@
 using System.Globalization;
 using Azure.Storage.Blobs;
+using GBB.Miyagi.RecommendationService.config;
 using GBB.Miyagi.RecommendationService.Utils;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.Memory.Qdrant;
@@ -13,7 +14,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -21,33 +21,15 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton(x => new BlobServiceClient(Env.Var("AZURE_STORAGE_CONNECTION_STRING")));
 
 
+
 // Register the required services
 builder.Services.AddSingleton<IKernel>(provider =>
 {
-    var kernel = Kernel.Builder
-        .WithLogger(ConsoleLogger.Log)
-        .Configure(c =>
-        {
-            if (Env.Var("USE_OPEN_AI") != null)
-            {
-                c.AddOpenAITextEmbeddingGenerationService("ada", "text-embedding-ada-002", Env.Var("OPENAI_API_KEY"));
-                c.AddOpenAITextCompletionService("davinci", "text-davinci-003", Env.Var("OPENAI_API_KEY"));
-            }
-            else
-            {
-                c.AddAzureTextCompletionService(
-                    Env.Var("AZURE_OPENAI_SERVICE_ID"),
-                    Env.Var("AZURE_OPENAI_DEPLOYMENT_NAME"),
-                    Env.Var("AZURE_OPENAI_ENDPOINT"),
-                    Env.Var("AZURE_OPENAI_KEY"));
-                c.AddAzureTextEmbeddingGenerationService(
-                    Env.Var("AZURE_OPENAI_EMBEDDINGS_SERVICE_ID"),
-                    Env.Var("AZURE_OPENAI_EMBEDDINGS_DEPLOYMENT_NAME"),
-                    Env.Var("AZURE_OPENAI_EMBEDDINGS_ENDPOINT"),
-                    Env.Var("AZURE_OPENAI_EMBEDDINGS_KEY"));
-            }
-        })
-        // swap with pgvector, acs, redis etc.
+    // initialize the kernel
+    var kernelSettings = KernelSettings.LoadSettings();
+    IKernel kernel = new KernelBuilder()
+            .WithCompletionService(kernelSettings)
+            // swap with pgvector, acs, redis etc.
         .WithMemoryStorage(new VolatileMemoryStore())
         .Configure(c => c.SetDefaultHttpRetryConfig(new HttpRetryConfig
         {
