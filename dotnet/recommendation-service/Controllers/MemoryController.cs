73809da5f6c197f71bfd3a1f304236818a6271fa
Govind Kamtamneni <gok@microsoft.com>
2023-07-03 18:14:54 -0700

    introduce text chunker for dataset embeddings

--- dotnet/recommendation-service/Controllers/MemoryController.cs
+++ dotnet/recommendation-service/Controllers/MemoryController.cs
@@ -3,15 +3,21 @@ using GBB.Miyagi.RecommendationService.models;
 using GBB.Miyagi.RecommendationService.Utils;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Text;
 
 namespace GBB.Miyagi.RecommendationService.Controllers;
 
+/// <summary>
+/// Controller for hydrating vector database with proprietary datasets
+/// </summary>
 [ApiController]
 [Route("memory")]
 public class MemoryController : ControllerBase
 {
     private readonly BlobServiceClient _blobServiceClient;
     private readonly IKernel _kernel;
+    private const int MaxTokensPerParagraph = 100;
+    private const int MaxTokensPerLine = 30;
     
     public MemoryController(IKernel kernel, BlobServiceClient blobServiceClient)
     {
@@ -19,9 +25,23 @@ public class MemoryController : ControllerBase
         _blobServiceClient = blobServiceClient;
     }
     
-    [HttpGet("/collections/{containerName}")]
-    public async Task<IActionResult> ListBlobsAsync(string containerName)
+    [HttpGet("/datasets/{containerName?}")]
+    public async Task<IActionResult> ListDatasets(string containerName = null)
     {
+        if (string.IsNullOrEmpty(containerName))
+        {
+            // Read files from local file system
+            var directoryPath = Path.Combine("resources", "sample-datasets");
+            if (Directory.Exists(directoryPath))
+            {
+                var files = Directory.GetFiles(directoryPath, "*.txt");
+                return Ok(files);
+            }
+
+            return NotFound($"Directory {directoryPath} not found.");
+        }
+
+        // Read files from Azure Blob Storage
         var containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
         var blobs = new List<string>();
 
@@ -30,21 +50,50 @@ public class MemoryController : ControllerBase
         return Ok(blobs);
     }
 
-    [HttpPost("/collections")]
-    public async Task<IActionResult> SendBlobToQdrantAsync([FromBody] SubRedditBlobInfo subRedditBlobInfo)
+
+    [HttpPost("/datasets")]
+    public async Task<IActionResult> SaveDatasetAsync([FromBody] DatasetInfo datasetInfo)
     {
-        var containerClient = _blobServiceClient.GetBlobContainerClient(subRedditBlobInfo.ContainerName);
-        var blobClient = containerClient.GetBlobClient(subRedditBlobInfo.BlobName);
+        string text;
+        if (string.IsNullOrEmpty(datasetInfo.BlobContainerName))
+        {
+            // Read file from local file system
+            var filePath = Path.Combine("resources", "sample-datasets", $"{datasetInfo.DataSetName}.txt");
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound($"File {filePath} not found.");
+            }
+            using var streamReader = new StreamReader(filePath);
+            text = await streamReader.ReadToEndAsync();
+        }
+        else
+        {
+            // Read file from Azure Blob Storage
+            var containerClient = _blobServiceClient.GetBlobContainerClient(datasetInfo.BlobContainerName);
+            var blobClient = containerClient.GetBlobClient(datasetInfo.DataSetName);
 
-        var response = await blobClient.DownloadAsync();
-        using var streamReader = new StreamReader(response.Value.Content);
-        var text = await streamReader.ReadToEndAsync();
+            var response = await blobClient.DownloadAsync();
+            using var streamReader = new StreamReader(response.Value.Content);
+            text = await streamReader.ReadToEndAsync();
+        }
 
-        // Save the text to Qdrant
+        // Chunk, generate embeddings, and persist to vectordb
         var memoryCollectionName = Env.Var("MEMORY_COLLECTION");
-        var key = await _kernel.Memory.SaveInformationAsync(memoryCollectionName, id: subRedditBlobInfo.BlobName,
-            text: text);
+        
+        var lines = TextChunker.SplitPlainTextLines(text, MaxTokensPerLine);
+        var chunks = TextChunker.SplitPlainTextParagraphs(lines, MaxTokensPerParagraph);
 
-        return Ok(new { Id = key });
+        for (var i = 0; i < chunks.Count; i++)
+        {
+            var chunk = chunks[i];
+            await _kernel.Memory.SaveInformationAsync(
+                collection: memoryCollectionName,
+                text: chunk,
+                id: $"{datasetInfo.DataSetName}-{i}",
+                description: $"Dataset: {datasetInfo.DataSetName}");
+        }
+
+        return Ok(new { Id = datasetInfo.DataSetName });
     }
+
 }
---
+++ dotnet/recommendation-service/Models/DatasetInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace GBB.Miyagi.RecommendationService.models;
+
+public class DatasetInfo
+{
+    public string DataSetName { get; set; }
+    public string? BlobContainerName { get; set; }
+    
+    public UserInfo? Metadata { get; set; }
+}
