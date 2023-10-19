4b50f19ca7bd8ff204d466569e84a9a1735dca08
ajai-d <106365942+ajai-d@users.noreply.github.com>
2023-10-19 18:46:08 -0500

    added additional azure services

--- deploy/infrastructure/cli/deploy.ps1
+++ deploy/infrastructure/cli/deploy.ps1
@@ -22,6 +22,11 @@ $skipOpenAI = "false"
 $skipEmbeddingModelDeployment = "false"
 $skipCompletionModelDeployment = "false"
 $skipcognitiveSearch = "false"
+$skipCosmosDB = "false"
+$skipBlobStorage = "false"
+$skipAzureContainerApps = "false"
+$skipAzureContainerRegistry = "false"
+$skipAPIM = "false"
 
 
 
@@ -75,6 +80,8 @@ for ($i = 1; $i -le $rgIndex; $i++) {
             --model-name text-embedding-ada-002 `
             --model-version "2"  `
             --model-format "OpenAI" `
+            --scale-capacity "20" `
+            --capacity "20"
     
     }
     
@@ -95,7 +102,108 @@ for ($i = 1; $i -le $rgIndex; $i++) {
             --model-name "gpt-35-turbo" `
             --model-version "0613"  `
             --model-format "OpenAI" `
+            --capacity "30"
       
+    }
+
+    # if skipCosmosDB is false, create CosmosDB account called miyagi with a container called recommendations
+
+    if ($skipCosmosDB -eq "true") {
+        Write-Host "Skipping CosmosDB account creation"
+    }
+    else {
+        Write-Host "Creating CosmosDB account $resourceGroupPrefix-cosmos-$i in $resourceGroupPrefix-rg-$i"
+        
+        az cosmosdb create `
+            --name "$resourceGroupPrefix-cosmos-$i" `
+            --resource-group "$resourceGroupPrefix-rg-$i" `
+            --kind "GlobalDocumentDB" `
+            --subscription $subscriptionId
+
+        Write-Host "Creating CosmosDB database $resourceGroupPrefix-cosmos-$i in $resourceGroupPrefix-rg-$i"
+        
+        az cosmosdb sql database create `
+            --account-name "$resourceGroupPrefix-cosmos-$i" `
+            --name "miyagi" `
+            --resource-group "$resourceGroupPrefix-rg-$i" `
+            --subscription $subscriptionId
+
+        Write-Host "Creating CosmosDB container $resourceGroupPrefix-cosmos-$i in $resourceGroupPrefix-rg-$i"
+        
+        az cosmosdb sql container create `
+            --account-name "$resourceGroupPrefix-cosmos-$i" `
+            --database-name "miyagi" `
+            --name "recommendations" `
+            --partition-key-path "/partitionKey" `
+            --resource-group "$resourceGroupPrefix-rg-$i" `
+            --subscription $subscriptionId
+    }
+
+    # if skipBlobStorage is false, create blob storage account with a container called miyagi
+
+    if ($skipBlobStorage -eq "true") {
+        Write-Host "Skipping blob storage account creation"
+    }
+    else {
+        
+        $storageaccount = -join($resourceGroupPrefix,"blobstorge",$i)
+        Write-Host "Creating blob storage account $storageaccount in $resourceGroupPrefix-rg-$i"
+        
+        az storage account create `
+            --name $storageaccount `
+            --resource-group "$resourceGroupPrefix-rg-$i" `
+            --location $location `
+            --sku "Standard_LRS" `
+            --subscription $subscriptionId
+
+        Write-Host "Creating blob storage container miyagi in $resourceGroupPrefix-rg-$i"
+        
+        az storage container create `
+            --name "miyagi" `
+            --account-name $storageaccount `
+            --subscription $subscriptionId
+    }
+
+    # if skipAzureContainerRegistry is false, create Azure Container Registry called miyagi
+
+    if ($skipAzureContainerRegistry -eq "true") {
+        Write-Host "Skipping Azure Container Registry creation"
+    }
+    else {
+        
+        $containerregistry = -join($resourceGroupPrefix,"acr",$i)
+        Write-Host "Creating Azure Container Registry $containerregistry in $resourceGroupPrefix-rg-$i"
+        
+        az acr create `
+            --name $containerregistry `
+            --resource-group "$resourceGroupPrefix-rg-$i" `
+            --location $location `
+            --sku "Basic" `
+            --subscription $subscriptionId
+    }
+
+    # if skipAzureContainerApps is false, create Azure Container Apps with a container called miyagi
+
+    if ($skipAzureContainerApps -eq "true") {
+        Write-Host "Skipping Azure Container App creation"
+    }
+    else {
+        
+        $containerapp = -join($resourceGroupPrefix,"miyagi",$i)
+        Write-Host "Creating Azure Container Apps $containerapp in $resourceGroupPrefix-rg-$i"
+
+        az containerapp up --name $containerapp  `
+        --resource-group "$resourceGroupPrefix-rg-$i" `
+        --location centralus `
+        --environment "$resourceGroupPrefix-env" `
+        --image mcr.microsoft.com/azuredocs/containerapps-helloworld:latest `
+        --target-port 80 `
+        --ingress external `
+        --query properties.configuration.ingress.fqdn `
+
+
+        
+        
     }
 
     # if skipcognitiveSearch is false, create cognitive search service with semantic search capability
@@ -114,6 +222,10 @@ for ($i = 1; $i -le $rgIndex; $i++) {
             
 
     }
+
+  
+
+
 
  
 
